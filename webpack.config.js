0cd94e3152121b02c3966b71658b842b4c158302
Devante Holston <dholston@doterra.com>
2023-12-01 16:34:56 -0600

    DH master: v1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
-'use strict';
-const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },{
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ],
+    },
     resolve: {
-        extensions: [".ts", ".tsx", ".jsx", ".js"]
+        extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+        open: true,
     },
 };
